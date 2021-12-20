887eeda3bd9b3c22eec41299dbf4c018ef8a6e92
SanviDomkondwar <96423394+SanviDomkondwar@users.noreply.github.com>
2021-12-20 17:41:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function calculate() {
+
+    var number_1 = Number(document.getElementById("firstno").value);
+  
+  var area = number_1 * number_1;
+  var perimeter  = 4 * number_1;
+  
+  document.getElementById("lblArea").innerHTML = area;
+  
+   /*Display the variable perimeter on the division with ID lblPerimeter.*/
+  document.getElementById("lblPerimeter").innerHTML = perimeter;
+    
+  }
+  
+  
+  
